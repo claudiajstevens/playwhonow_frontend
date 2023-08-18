64e4a1394e284e8d40ceb9566d186f0a13fd39ff
claudiajstevens <claudiajstevens@gmail.com>
2023-08-18 15:59:40 -0700

    feat: saved jwt token in local storage

--- src/App.js
+++ src/App.js
@@ -12,14 +12,13 @@ import NavBar from './components/NavBar';
 import Hero from './components/Hero/Hero';
 import Home from './views/Home';
 import Profile from './views/Profile';
+import { useLocalState } from './util/useLocalStorage';
 
 
 function App() {
+  // may want to save jwt in cookie instead of local storage
+  const [jwt, setJwt] = useLocalState("", "jwt");
 
-  const  reqBody = {
-    "username": "cloud",
-    "password": "password"
-  }
 
   // fetch("/auth/register", {
   //   headers: {
@@ -29,26 +28,42 @@ function App() {
   //   body: JSON.stringify(reqBody)
   // });
 
+  // fetch returns a promise
+  // the array of the second argument is dependencies 
   useEffect( () => {
-    fetch("/auth/login", {
-      headers: {
-        "Content-Type": "application/json"
+    if( !jwt ){
+      const  reqBody = {
+        "username": "cloud",
+        "password": "password"
+      };
+
+      fetch("/auth/login", {
+        headers: {
+          "Content-Type": "application/json"
       },
       method: "post",
       body: JSON.stringify(reqBody)
-    })
-    .then(response => Promise.all([response.json(), response.headers]))
-    .then(([body, headers]) => console.log(body.jwt));
+      })
+      .then(response => Promise.all([response.json(), response.headers]))
+      .then(([body, headers]) => {
+        setJwt(body.jwt);  
+      });
+    }
+
   }, []);
+
+  useEffect( () => {
+    console.log(`JWT is: ${jwt}`);
+  }, [jwt]);
 
 
   //adding connection to spring boot application
   // fetch returns a promise
-  useEffect( () => {
-    fetch('/hello?name="Cloud"')
-    .then(response=>response.text())
-    .then(result=>console.log(result));
-  }, []);
+  // useEffect( () => {
+  //   fetch('/hello?name="Cloud"')
+  //   .then(response=>response.text())
+  //   .then(result=>console.log(result));
+  // }, []);
 
   return (
     <div>
